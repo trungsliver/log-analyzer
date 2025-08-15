1d9bdeccd53db33f99c341f99e3daf87b8c86aee
trungsliver <buiductrung.cva@gmail.com>
2025-08-15 15:27:10 +0700

    concurrency: read counter

--- src/main/java/concurrency/ReadCounter.java
+++ src/main/java/concurrency/ReadCounter.java
@@ -1,4 +1,19 @@
 package concurrency;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+// Concurrency dùng để xử lý nhiều tác vụ đồng thời
+// concurrency.ReadCounter là một lớp đơn giản để đếm số lần đọc trong môi trường đa luồng.
+
 public class ReadCounter {
+    // atomic counter là một biến đếm được sử dụng trong môi trường đa luồng mà không cần phải sử dụng synchronized hay locks.
+    private AtomicInteger count = new AtomicInteger(0);
+
+    public void increment() {
+        count.incrementAndGet();
+    }
+
+    public int getCount() {
+        return count.get();
+    }
 }
